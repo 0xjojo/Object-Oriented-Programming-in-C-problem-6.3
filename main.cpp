6d56b4cc833a628cbb83a97a3b94cafb60766249
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2018-05-03 21:56:09 +0200

    Problem 6.3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-
-using namespace std;
 /*3. Create a class called time that has separate int member data for hours, minutes, and
 seconds. One constructor should initialize this data to 0, and another should initialize it
 to fixed values. Another member function should display it, in 11:59:59 format. The final
@@ -9,6 +6,10 @@ A main() program should create two initialized time objects (should they be cons
 one that isn’t initialized. Then it should add the two initialized values together, leaving the
 result in the third time variable. Finally it should display the value of this third variable.
 Make appropriate member functions const.*/
+#include <iostream>
+
+using namespace std;
+
 class time {
     private :
         int hour , minute , second ;
